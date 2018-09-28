fbc3a55ab067b49a7728a0d4471220877c483423
Phillip Johnsen <phillip.johnsen@tomra.com>
2018-09-28 14:28:51 +0200

    initial experimental commit

---
+++ index.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const child_process = require("child_process");
+
+class ServerlessReasonMlPlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+
+    this.hooks = {
+      "before:package:createDeploymentArtifacts": this.bsbMakeWorld.bind(this),
+      "before:offline:start": this.bsbWatch.bind(this),
+      "before:offline:start:init": this.bsbWatch.bind(this)
+    };
+  }
+
+  bsbWatch() {
+    return this.bsbMakeWorld().then(() => {
+      this.serverless.cli.log("Watching for changes...");
+
+      this._bsbWatcher = child_process.spawn(
+        "./node_modules/.bin/bsb",
+        ["-w"],
+        { cwd: process.cwd(), stdio: "inherit" }
+      );
+
+      this._bsbWatcher.on("error", err => {
+        console.error("Got bsb watching error!", err);
+      });
+    });
+  }
+
+  bsbMakeWorld() {
+    return new Promise((resolve, reject) => {
+      this.serverless.cli.log("Building reasonml files...");
+
+      child_process
+        .spawn("./node_modules/.bin/bsb", ["-make-world"], {
+          cwd: process.cwd(),
+          stdio: "inherit"
+        })
+        .on("error", err => reject(err))
+        .on('close', exitCode => exitCode === 0 ? resolve() : reject(new Error('Failed with code ' + exitCode));
+    });
+  }
+}
+
+module.exports = ServerlessReasonMlPlugin;
